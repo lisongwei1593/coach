a6673653b0ef651c307c7da1064f46824e1277c8
lisongwei1593 <lisongwei1593@gmail.com>
2016-03-25 22:25:16 +0800

    UPD:add reverse

--- reserve/views.py
+++ reserve/views.py
@@ -1,7 +1,8 @@
-from django.http.response import HttpResponseRedirect
+# -*- coding: utf-8 -*-s
+from django.http.response import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
-
-from reserve.models import Coach, ReserveTime, ReserveInterval, Student
+import datetime,traceback
+from reserve.models import Coach, ReserveTime, ReserveInterval, Student, Reserve
 
 
 # Create your views here.
@@ -14,12 +15,23 @@ def index(request):
 
 def set_interval(request,minutes):
     ReserveInterval.objects.create(interval=int(minutes))
-    return HttpResponseRedirect("/")  
+    return HttpResponseRedirect("/")
 
 
 def reserve(request):
     if request.method == 'POST':
-        print "ss"
-#         ReserveInterval.objects.create(interval=1)
-    return HttpResponseRedirect("/") 
-    
+        try:
+            coach_id = request.POST.get("coach_id")
+            coach = Coach.objects.get(id=coach_id)
+            time_str = request.POST.get('time')
+            reserve_time = datetime.datetime.strptime(time_str, '%H:%M')
+            endtime = reserve_time + datetime.timedelta(hours=1)
+            #省略了登陆注册，此处默认为学员A
+            student = Student.objects.get(id=1)
+            Reserve.objects.create(coach=coach,starttime=reserve_time.time(),
+                                   endtime=endtime.time(),student=student,reserve_date=datetime.datetime.today())
+            return HttpResponse("ok")
+        except:
+            traceback.print_exc()
+            return HttpResponse(u"预约失败")
+
